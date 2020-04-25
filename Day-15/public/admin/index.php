281cbce384c8d00a2dc67285d7a1fe283ee2b058
Khalil Minalang <minalang.khalil@gmail.com>
2020-04-26 07:20:41 +0800

    Finished tutorial: 12. Late static binding

---
+++ Day-15/public/admin/index.php
@@ -0,0 +1,13 @@
+<?php
+	// require_once("../../includes/functions.php");
+	// require_once("../../includes/session.php");
+	require_once("../../includes/initialize.php");
+
+	// 
+	if (!$session->is_logged_in()) { redirect_to('login.php'); }
+?>
+
+<?php include_layout_template('admin_header.php'); ?>
+			<h2>Menu</h2>
+		<!-- </div> -->
+<?php include_layout_template('admin_footer.php'); ?>
